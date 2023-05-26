d4b0b4cc83650c60e31f643c928dace442ed7121
Daniel Andrews <daniel.korienko@gmail.com>
2023-05-26 14:08:21 +0300

    Added cubatic bezier curve

--- Run-N-Stop/Assets/_Scripts/Bezier.cs
+++ Run-N-Stop/Assets/_Scripts/Bezier.cs
@@ -5,6 +5,7 @@ public class Bezier : MonoBehaviour
     [SerializeField] private Transform _pointA;
     [SerializeField] private Transform _pointB;
     [SerializeField] private Transform _pointC;
+    [SerializeField] private Transform _pointD;
 
     private float _t;
 
@@ -12,7 +13,7 @@ public class Bezier : MonoBehaviour
     {
         _t = (_t % 1) + Time.deltaTime;
 
-        transform.position = QuadraticBezier(_pointA.position, _pointB.position, _pointC.position, _t);
+        transform.position = CubicBezier(QuadraticBezier(_pointA.position, _pointB.position, _pointC.position, _t), QuadraticBezier(_pointB.position, _pointC.position, _pointD.position, _t), _t);
     }
 
     private Vector3 LinearBezier(Vector3 pointA, Vector3 pointB, float t)
@@ -22,6 +23,11 @@ public class Bezier : MonoBehaviour
 
     private Vector3 QuadraticBezier(Vector3 pointA, Vector3 pointB, Vector3 pointC, float t)
     {
-        return (1 - t) * ((1 - t) * pointA + (t) * pointB) + t * ((1 - t) * pointB + t * pointC);
+        return (1 - t) * ((1 - t) * pointA + t * pointB) + t * ((1 - t) * pointB + t * pointC);
+    }
+
+    private Vector3 CubicBezier(Vector3 pointA, Vector3 pointB, float t)
+    {
+        return (1 - t) * pointA + t * t * pointB;
     }
 }
