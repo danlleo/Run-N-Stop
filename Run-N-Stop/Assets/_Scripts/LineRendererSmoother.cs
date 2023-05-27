a64c26dfed06f27d3d8ad973ec519b832688d4f6
Daniel Andrews <daniel.korienko@gmail.com>
2023-05-27 19:49:41 +0300

    Bezier Curve

---
+++ Run-N-Stop/Assets/_Scripts/LineRendererSmoother.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LineRendererSmoother : MonoBehaviour
+{
+    public LineRenderer Line;
+    public Vector3[] InitialState;
+
+    public float SmoothingLength = 2f;
+    public int SmoothingSections = 10;
+}
